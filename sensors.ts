b595c8dc16b0474f37bdd967985772bc657e772c
perguto <perguto@protonmail.com>
2023-07-10 01:05:41 +0200

    f

--- sensors.ts
+++ sensors.ts
@@ -1,18 +1,15 @@
-const acl = new Accelerometer({ frequency: 60 });
+const acl = new Accelerometer({ frequency: 60 })
 const acl_x_display = document.getElementById('acl_x')
 const acl_y_display = document.getElementById('acl_y')
 const acl_z_display = document.getElementById('acl_z')
 acl.addEventListener("reading", () => {
   console.log(`Acceleration along the X-axis ${acl.x}`);
-	const precision = 4
-	acl_x_display.textContent = acl.x.toFixed(precision)
-
+	acl_x_display.textContent = acl.x
   console.log(`Acceleration along the Y-axis ${acl.y}`);
-	acl_y_display.textContent = acl.y.toFixed(precision)
+	acl_y_display.textContent = acl.y
   console.log(`Acceleration along the Z-axis ${acl.z}`);
-	acl_z_display.textContent = acl.z.toFixed(precision)
+	acl_z_display.textContent = acl.z
 });
 
 acl.start();
-
 

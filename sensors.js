c0a4f5bfecbbab012a0d1947e1b7868fe3b85379
perguto <perguto@protonmail.com>
2023-07-10 00:53:18 +0200

    f

---
+++ sensors.js
@@ -0,0 +1,15 @@
+const acl = new Accelerometer({ frequency: 60 });
+const acl_x_display = document.getElementById('acl_x')
+const acl_y_display = document.getElementById('acl_y')
+const acl_z_display = document.getElementById('acl_z')
+acl.addEventListener("reading", () => {
+  console.log(`Acceleration along the X-axis ${acl.x}`);
+	acl_x_display.textContent = acl.x
+  console.log(`Acceleration along the Y-axis ${acl.y}`);
+	acl_y_display.textContent = acl.y
+  console.log(`Acceleration along the Z-axis ${acl.z}`);
+	acl_z_display.textContent = acl.z
+});
+
+acl.start();
+

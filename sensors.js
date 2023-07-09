d572e2f21e37655b76c0bbd3f263f7ab35197cec
perguto <perguto@protonmail.com>
2023-07-10 01:10:11 +0200

    f

--- sensors.js
+++ sensors.js
@@ -5,12 +5,12 @@ const acl_z_display = document.getElementById('acl_z')
 acl.addEventListener("reading", () => {
   console.log(`Acceleration along the X-axis ${acl.x}`);
 	const precision = 4
-	acl_x_display.textContent = acl.x.toFixed(precision)
+	acl_x_display.textContent = acl.x.toFixed(precision).padStart(3+precision,'0')
 
   console.log(`Acceleration along the Y-axis ${acl.y}`);
-	acl_y_display.textContent = acl.y.toFixed(precision)
+	acl_y_display.textContent = acl.y.toFixed(precision).padStart(3+precision,'0')
   console.log(`Acceleration along the Z-axis ${acl.z}`);
-	acl_z_display.textContent = acl.z.toFixed(precision)
+	acl_z_display.textContent = acl.z.toFixed(precision).padStart(3+precision,'0')
 });
 
 acl.start();
